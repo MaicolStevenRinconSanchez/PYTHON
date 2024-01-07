21f81b3e84af231bddc162fb3b2fd82c171c64f7
Maicol Steven Rincón Sanchez <143055463+MaicolStevenRinconSanchez@users.noreply.github.com>
2024-01-06 23:08:10 -0500

    Add files via upload

---
+++ python/CAMPUSLAND_J1/Registro_Estudiantil/REGISTRO_ESTUDIANTIL.py
@@ -0,0 +1,24 @@
+import IMC
+import os
+
+registro_estudiantil = []
+
+# Recopilar datos de estudiantes
+for i in range(2):  # Asumiendo que queremos registrar 2 estudiantes
+    print(f"Datos del estudiante {i + 1}:")
+    estudiante = IMC.salud()
+    registro_estudiantil.append(estudiante)
+    os.system("clear")
+
+# Calcular el IMC para cada estudiante
+for estudiante in registro_estudiantil:
+    resultado_imc = IMC.imc(estudiante['nombre'], estudiante['edad'], estudiante['altura'], estudiante['peso'])
+    print(f"Estudiante {estudiante['nombre']} - {resultado_imc}")
+#3. Teniendo en cuenta el punto 2 se requiere tener el registro de 20 estudiantes y poder determinar
+#el estado de salud de la comunidad estudiantil. El programa debe mostrar el siguiente reporte:}
+
+# a. Cuantos estudiantes se encuentran en el peso ideal.
+# b. Cuantos estudiantes se encuentran en obesidad grado I
+# c. Cuantos estudiantes se encuentran en obesidad grado II
+# d. Cuantos estudiantes se encuentran en obesidad grado III
+# e. Cuantos estudiantes se encuentran en Sobrepeso 
